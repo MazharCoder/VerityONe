4a8bbbc6b26ae25162d8d6bc59ae66f890ff6c1a
MazharCoder <mazharitzone@gmail.com>
2025-05-19 19:26:25 +0500

    changes in sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -28,7 +28,7 @@ const Sidebar = () => {
           <div className="bg-blue-600 text-white p-2 rounded-md mr-2">
             <ClipboardCheck size={24} />
           </div>
-          <h1 className="text-xl font-bold text-gray-800">VerityONE</h1>
+          <h1 className="text-xl font-bold text-gray-800">My Project for details</h1>
         </div>
       </div>
       <nav className="p-4">
